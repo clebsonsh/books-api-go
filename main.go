aa73f33a6ce21621e548b0610967e77709bf84b9
Clebson Moura <clebson.sh@gmail.com>
2024-02-18 20:41:32 -0300

    Add command line arguments to handle database migrations and seeding

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"os"
+
 	"github.com/clebsonsh/books-api-go/database"
+	"github.com/clebsonsh/books-api-go/scripts"
 	"github.com/clebsonsh/books-api-go/utils"
 )
 
 func init() {
 	utils.LoadEnv()
 	database.Init()
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "migrate:fresh:seed":
+			scripts.Migrate(true)
+			scripts.Seed()
+		case "migrate:fresh":
+			scripts.Migrate(true)
+		case "migrate":
+			scripts.Migrate(false)
+		case "seed":
+			scripts.Seed()
+		}
+	}
 }
 
 func main() {
