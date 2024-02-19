04a1473a7a231efb9ed71d21031d81e59bc44ea7
Clebson Moura <clebson.sh@gmail.com>
2024-02-18 21:37:27 -0300

    Add API endpoints for books and authors

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 
-	"github.com/clebsonsh/books-api-go/database"
+	"github.com/clebsonsh/books-api-go/data"
 	"github.com/clebsonsh/books-api-go/scripts"
 	"github.com/clebsonsh/books-api-go/utils"
 )
 
 func init() {
 	utils.LoadEnv()
-	database.Init()
 
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
@@ -28,5 +29,29 @@ func init() {
 }
 
 func main() {
+	serverUriAndPort := os.Getenv("HOST_URI") + ":" + os.Getenv("HOST_PORT")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/v1/books/", func(w http.ResponseWriter, r *http.Request) {
+		books, err := data.GetBooks()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+		utils.RespondWithJSON(w, books, http.StatusOK)
+	})
+
+	mux.HandleFunc("GET /api/v1/authors/", func(w http.ResponseWriter, r *http.Request) {
+		authors, err := data.GetAuthors()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		utils.RespondWithJSON(w, authors, http.StatusOK)
+	})
+
+	fmt.Printf("Server running at http://%s\n", serverUriAndPort)
+
+	http.ListenAndServe(serverUriAndPort, mux)
 }
